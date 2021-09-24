a122bf4a2680b2d70712e1f4513233926369323a
yrc <yrc@servyou.com.cn>
2021-09-24 21:06:35 +0800

    bugfix

--- src/main/java/com/oath/maven/plugin/freemarker/JsonPropertiesProvider.java
+++ src/main/java/com/oath/maven/plugin/freemarker/JsonPropertiesProvider.java
@@ -12,65 +12,89 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
 public class JsonPropertiesProvider implements OutputGeneratorPropertiesProvider {
-	private final Gson gson;
-	private final Type stringObjectMap;
-	private final File dataDir;
-	private final File templateDir;
-	private final File outputDir;
+    private final Gson gson;
+    private final Type stringObjectMap;
+    private final File dataDir;
+    private final File templateDir;
+    private final File outputDir;
+    private File basedir;
 
-	private JsonPropertiesProvider(File dataDir, File templateDir, File outputDir) {
-		this.dataDir = dataDir;
-		this.templateDir = templateDir;
-		this.outputDir = outputDir;
-		gson = new GsonBuilder().setLenient().create();
-		stringObjectMap = new TypeToken<Map<String, Object>>() { } .getType();
-	}
+    private JsonPropertiesProvider(File dataDir, File templateDir, File outputDir) {
+        this.dataDir = dataDir;
+        this.templateDir = templateDir;
+        this.outputDir = outputDir;
+        gson = new GsonBuilder().setLenient().create();
+        stringObjectMap = new TypeToken<Map<String, Object>>() {
+        }.getType();
+    }
 
-	public static JsonPropertiesProvider create(File dataDir, File templateDir, File outputDir) {
-		return new JsonPropertiesProvider(dataDir, templateDir, outputDir);
-	}
+    public static JsonPropertiesProvider create(File dataDir, File templateDir, File outputDir) {
+        return new JsonPropertiesProvider(dataDir, templateDir, outputDir);
+    }
 
-	@Override
-	public void providePropertiesFromFile(Path path, OutputGenerator.OutputGeneratorBuilder builder) {
-		File jsonDataFile = path.toFile();
-		Map<String,Object> data = parseJson(jsonDataFile);
+    public void setBasedir(File basedir) {
+        this.basedir = basedir;
+    }
 
-		Object obj = data.get("dataModel");
-		if (obj != null) {
-			builder.addDataModel((Map<String, Object>) obj);
-		} else {
-			builder.addDataModel(new HashMap<String,Object>());
-		}
+    @Override
+    public void providePropertiesFromFile(Path path, OutputGenerator.OutputGeneratorBuilder builder) {
+        File jsonDataFile = path.toFile();
+        Map<String, Object> data = parseJson(jsonDataFile);
 
-		obj = data.get("templateName");
-		if (obj == null) {
-			throw new RuntimeException("Require json data property not found: templateName");
-		}
-		builder.addTemplateLocation(templateDir.toPath().resolve(obj.toString()));
+        Object obj = data.get("dataModel");
+        if (obj != null) {
+            builder.addDataModel((Map<String, Object>) obj);
+        } else {
+            builder.addDataModel(new HashMap<String, Object>());
+        }
 
-		String dataDirName = dataDir.getAbsolutePath();
-		String jsonFileName = jsonDataFile.getAbsolutePath();
-		if (!jsonFileName.startsWith(dataDirName)) {
-			throw new IllegalStateException("visitFile() given file not in sourceDirectory: " + jsonDataFile);
-		}
+        obj = data.get("templateName");
+        if (obj == null) {
+            throw new RuntimeException("Require json data property not found: templateName");
+        }
+        builder.addTemplateLocation(templateDir.toPath().resolve(obj.toString()));
 
-		String outputFileName = jsonFileName.substring(dataDirName.length()+1);
-		outputFileName = outputFileName.substring(0, outputFileName.length() - 5);
-		Path outputPath = outputDir.toPath();
-		Path resolved = outputPath.resolve(outputFileName);
-		builder.addOutputLocation(resolved);
-	}
+        String dataDirName = dataDir.getAbsolutePath();
+        String jsonFileName = jsonDataFile.getAbsolutePath();
+        if (!jsonFileName.startsWith(dataDirName)) {
+            throw new IllegalStateException("visitFile() given file not in sourceDirectory: " + jsonDataFile);
+        }
 
-	private Map<String, Object> parseJson(File jsonDataFile) {
-		try (JsonReader reader = new JsonReader(new InputStreamReader(new FileInputStream(jsonDataFile), "UTF-8"))) {
-			return gson.fromJson(reader, stringObjectMap);
-		} catch (Throwable t) {
-			throw new RuntimeException("Could not parse json data file: " + jsonDataFile, t);
-		}
-	}
+        String outputFileName = jsonFileName.substring(dataDirName.length() + 1);
+        outputFileName = outputFileName.substring(0, outputFileName.length() - 5);
+
+        Object ftlOutputDir = data.get("outputDir");
+
+        Path outputPath = null;
+        if (ftlOutputDir != null) {
+            String subPath = ftlOutputDir.toString();
+            if (subPath.trim().length() > 0) {
+                outputPath = new File(basedir.getAbsolutePath() + "/" + subPath.trim()).toPath();
+            } else {
+                outputPath = basedir.toPath();
+            }
+        }
+
+        if (outputPath == null) {
+            outputPath = outputDir.toPath();
+        }
+        Path resolved = outputPath.resolve(outputFileName);
+
+
+        builder.addOutputLocation(resolved);
+    }
+
+    private Map<String, Object> parseJson(File jsonDataFile) {
+        try (JsonReader reader = new JsonReader(new InputStreamReader(new FileInputStream(jsonDataFile), StandardCharsets.UTF_8))) {
+            return gson.fromJson(reader, stringObjectMap);
+        } catch (Throwable t) {
+            throw new RuntimeException("Could not parse json data file: " + jsonDataFile, t);
+        }
+    }
 }
